894bf1002c92867b8beb0d24389c377d7a01b8a3
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-26 03:12:31 +0000

    fixing min+max

--- src/max.ts
+++ src/max.ts
@@ -8,10 +8,7 @@ export function max<T, R>(keySelector?: (value: T) => R): Operation<T, R>;
  */
 export function max<T, R>(keySelector?: (value: T) => R): Operation<T, T | R> {
     const cb = keySelector ? (p: T, c: T, idx: number) => {
-        if (idx > 1) {
-            return p;
-        }
-        const a = keySelector(p);
+        const a = idx > 1 ? p : keySelector(p);
         const b = keySelector(c);
         return a > b ? a : b;
     } : (p: T, c: T) => p > c ? p : c;
