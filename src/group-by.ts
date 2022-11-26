393ae51db5e06fd617da1e2bbae10092239f1ecf
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-26 05:19:29 +0000

    refactoring groupBy

--- src/group-by.ts
+++ src/group-by.ts
@@ -1,11 +1,13 @@
 import {Operation, reduce} from 'iter-ops';
 
+export type GroupByResult<T> = { [key: string]: T[] };
+
 /**
  * Groups objects by a property value.
  */
-export function groupBy<T>(predicate: (v: T) => string): Operation<T, { [key: string]: T[] }> {
-    return reduce((acc: { [key: string]: T[] }, value: T) => {
+export function groupBy<T>(predicate: (v: T) => string): Operation<T, GroupByResult<T>> {
+    return reduce((acc: GroupByResult<T>, value: T) => {
         (acc[predicate(value)] ||= []).push(value);
         return acc;
-    }, {} as { [key: string]: T[] });
+    }, {} as GroupByResult<T>);
 }
