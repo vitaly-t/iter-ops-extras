0e1044df4ffe0d2b22dafd0db8d44a0b22c0874f
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-26 01:48:44 +0000

    improving sum operator

--- src/sum.ts
+++ src/sum.ts
@@ -1,8 +1,14 @@
 import {Operation, reduce} from 'iter-ops';
 
+export function sum<T>(): Operation<T, T>;
+export function sum<T, R>(keySelector: (value: T) => R): Operation<T, R>;
+
 /**
- * Sums up a sequence of numbers.
+ * Sums up values, with optional key selector.
  */
-export function sum(): Operation<number, number> {
-    return reduce((p: number, c: number) => p + c);
+export function sum<T, R>(keySelector?: (value: T) => R): Operation<T, R> {
+    const cb = keySelector ? (p: T, c: T, idx: number) =>
+        (idx > 1 ? p : keySelector(p)) as any + keySelector(c) : (p: T, c: T) => p as any + c;
+
+    return reduce(cb);
 }
