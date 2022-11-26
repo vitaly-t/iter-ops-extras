c817b8d5b13d3f0fc573aa25fda33246f3f1b8bc
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-26 03:02:37 +0000

    improving miMax operator

--- src/min-max.ts
+++ src/min-max.ts
@@ -1,12 +1,28 @@
 import {Operation, reduce} from 'iter-ops';
 
+export type MinMaxResult<T> = { min?: T, max?: T };
+
+export function minMax<T>(): Operation<T, MinMaxResult<T>>;
+export function minMax<T, R>(keySelector?: (value: T) => R): Operation<T, MinMaxResult<R>>;
+
 /**
- * Emits {min, max} in one iteration.
+ * Emits {min, max} in one iteration, with optional key selector.
  */
-export function minMax<T>(): Operation<T, { min?: T, max?: T }> {
-    return reduce((p, c) => {
-        p.min = c < p.min ? c : p.min ?? c;
-        p.max = c > p.max ? c : p.max ?? c;
-        return p;
-    }, {min: undefined, max: undefined});
+export function minMax<T, R>(keySelector?: (value: T) => R): Operation<T, MinMaxResult<R>> {
+    let cb;
+    if (keySelector) {
+        cb = (p, c) => {
+            const a = keySelector(c);
+            p.min = a < p.min ? a : p.min ?? a;
+            p.max = a > p.max ? a : p.max ?? a;
+            return p;
+        }
+    } else {
+        cb = (p, c) => {
+            p.min = c < p.min ? c : p.min ?? c;
+            p.max = c > p.max ? c : p.max ?? c;
+            return p;
+        }
+    }
+    return reduce(cb, {min: undefined, max: undefined});
 }
