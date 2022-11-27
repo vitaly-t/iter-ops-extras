3ac0b1f2586f1e2a1b04a126f38e5dba67ae41dc
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-27 13:14:18 +0000

    reversing distinctValues change

--- src/distinct-values.ts
+++ src/distinct-values.ts
@@ -3,7 +3,7 @@ import {distinct, map, Operation} from 'iter-ops';
 /**
  * Selects unique key-values, then remaps them into just values.
  */
-export function distinctValues<T, R>(keySelector: (value: T) => R): Operation<T, R> {
+export function distinctValues<T, R>(keySelector: (value: T, index: number) => R): Operation<T, R> {
     return i => {
         const k = distinct(keySelector)(i);
         return map(keySelector)(k);
