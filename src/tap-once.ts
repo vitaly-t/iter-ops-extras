3662996d366f8d2da1729274df6cfb1524c84a8e
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-27 11:28:13 +0000

    reversing tapOnce change

--- src/tap-once.ts
+++ src/tap-once.ts
@@ -1,12 +1,12 @@
-import {IterationState, Operation, tap} from 'iter-ops';
+import {Operation, tap} from 'iter-ops';
 
 /**
  * Taps into the very first value only.
  */
-export function tapOnce<T>(cb: (value: T, state: IterationState) => void): Operation<T, T> {
-    return tap((value, idx, state) => {
+export function tapOnce<T>(cb: (value: T) => void): Operation<T, T> {
+    return tap((value, idx) => {
         if (!idx) {
-            cb(value, state);
+            cb(value);
         }
     });
 }
