e25a29a88e96ec5ff1d5e3d2c01e369cf6e18fdf
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-27 11:05:41 +0000

    improving operator tapOnce

--- src/tap-once.ts
+++ src/tap-once.ts
@@ -1,12 +1,12 @@
-import {Operation, tap} from 'iter-ops';
+import {IterationState, Operation, tap} from 'iter-ops';
 
 /**
  * Taps into the very first value only.
  */
-export function tapOnce<T>(cb: (value: T) => void): Operation<T, T> {
-    return tap((value, idx) => {
+export function tapOnce<T>(cb: (value: T, state: IterationState) => void): Operation<T, T> {
+    return tap((value, idx, state) => {
         if (!idx) {
-            cb(value);
+            cb(value, state);
         }
     });
 }
