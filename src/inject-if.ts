894d1128bbc00ee4bf4a8a7a69e01fe821f42b8a
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-29 13:14:35 +0000

    minor refactoring

--- src/inject-if.ts
+++ src/inject-if.ts
@@ -29,22 +29,22 @@ export function replaceIf<T, R>(value: SyncValue<R>, predicate: (value: T, index
  * Conditional injector of synchronous iterables, with support for asynchronous predicate result.
  */
 function injectIf<T, R>(predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>, cb: (current: T) => SyncValue<R>): Operation<T, T | R> {
-    return i => concurrencyFork({
-        onSync(source: Iterable<T>) {
-            const m = map((a: T, index, state) => predicate(a, index, state) ? cb(a) : [a])(source);
+    return source => concurrencyFork({
+        onSync(i: Iterable<T>) {
+            const m = map((a: T, index, state) => predicate(a, index, state) ? cb(a) : [a])(i);
             return spread()(m as Iterable<Array<T | R>>) as Iterable<T | R>;
         },
-        onAsync(source: AsyncIterable<T>) {
+        onAsync(i: AsyncIterable<T>) {
             const m = map((a: T, index, state) => {
                 const r = predicate(a, index, state) as any;
                 const isPromise = r && typeof r.then === 'function';
                 const out = (pass: boolean) => pass ? cb(a) : [a];
                 return isPromise ? r.then(out) : out(r);
-            })(source) as AsyncIterable<Array<T | R> | Promise<Array<T | R>>>;
+            })(i) as AsyncIterable<Array<T | R> | Promise<Array<T | R>>>;
             const w = wait()(m) as AsyncIterable<Array<T | R>>;
             return spread()(w) as AsyncIterable<T | R>;
         }
-    })(i);
+    })(source);
 }
 
 /**
