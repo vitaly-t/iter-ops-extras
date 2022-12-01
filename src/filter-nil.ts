96075d3f4c2bf14e8914dc52779aed15fe906fb1
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-01 08:47:25 +0000

    improving filterNil

--- src/filter-nil.ts
+++ src/filter-nil.ts
@@ -3,6 +3,6 @@ import {filter, Operation} from 'iter-ops';
 /**
  * Filters out all `null` and `undefined` values.
  */
-export function filterNil<T>(): Operation<T, T> {
-    return filter(a => a !== null && a !== undefined);
+export function filterNil<T>(): Operation<T | undefined | null, T> {
+    return filter((a: any) => a !== null && a !== undefined);
 }
