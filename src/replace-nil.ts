fffdfabffe522e96aa1efc684e67d3793b094d35
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-01 08:51:37 +0000

    improve replaceNil

--- src/replace-nil.ts
+++ src/replace-nil.ts
@@ -3,6 +3,6 @@ import {map, Operation} from 'iter-ops';
 /**
  * Replaces every `null` and `undefined` with an alternative value.
  */
-export function replaceNil<T, R>(value: R): Operation<T, T | R> {
+export function replaceNil<T, R>(value: R): Operation<T | undefined | null, T | R> {
     return map(a => a ?? value);
 }
