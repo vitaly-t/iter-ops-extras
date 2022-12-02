217c6c95d04e2b86788147638ff212227439ca05
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-02 12:53:46 +0000

    docs

--- src/to-readable.ts
+++ src/to-readable.ts
@@ -3,6 +3,16 @@ import {Readable} from 'stream';
 
 /**
  * Converts the iterable into a Readable stream, with optional callback.
+ *
+ * The optional callback is to let you set up event listeners:
+ *
+ * ```ts
+ * toReadable(r => {
+ *    r.once('close', () => {
+ *        // stream closed
+ *    })
+ * })
+ * ```
  */
 export function toReadable<T>(cb?: (stream: Readable) => void): Operation<T, Readable> {
     return source => consume(i => {
