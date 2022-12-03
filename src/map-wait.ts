f13ddcafee3512ad7c8c2e9bfb48d51042d178f4
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-03 07:58:03 +0000

    docs

--- src/map-wait.ts
+++ src/map-wait.ts
@@ -5,6 +5,9 @@ import {IterationState, Operation, concurrencyFork, map, wait} from 'iter-ops';
  *
  * It adds operator "wait" only when inside asynchronous pipeline.
  *
+ * The key difference from using "map" + "wait" is that operator "wait" will throw
+ * an error when used inside synchronous pipeline, and "mapWait" here will not.
+ *
  * See API for operator "map" that explains why it does not resolve promises by itself:
  * https://vitaly-t.github.io/iter-ops/functions/map
  */
