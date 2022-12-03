b84a521b245263b6f839bbf036f5822b0c684571
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-03 10:11:03 +0000

    refactoring injectors

--- src/inject-if.ts
+++ src/inject-if.ts
@@ -3,35 +3,35 @@ import {IterationState, Operation, map, spread, wait, concurrencyFork, toIterabl
 /**
  * Conditionally injects value(s) after the current value.
  */
-export function appendIf<T, R>(value: R | Iterable<R>, predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>): Operation<T, T | R> {
-    const i = toIterable(value as R);
+export function appendIf<T>(value: T | Iterable<T>, predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>): Operation<T, T> {
+    const i = toIterable(value as T);
     return injectIf(predicate, current => [current, ...i]);
 }
 
 /**
  * Conditionally injects value(s) before the current value.
  */
-export function prependIf<T, R>(value: R | Iterable<R>, predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>): Operation<T, T | R> {
-    const i = toIterable(value as R);
+export function prependIf<T>(value: T | Iterable<T>, predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>): Operation<T, T> {
+    const i = toIterable(value as T);
     return injectIf(predicate, current => [...i, current]);
 }
 
 /**
  * Conditionally injects value(s) in place of the current value.
  */
-export function replaceIf<T, R>(value: R | Iterable<R>, predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>): Operation<T, T | R> {
-    const i = toIterable(value as R);
+export function replaceIf<T>(value: T | Iterable<T>, predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>): Operation<T, T> {
+    const i = toIterable(value as T);
     return injectIf(predicate, () => i);
 }
 
 /**
  * Conditional injector of synchronous iterables, with support for asynchronous predicate result.
  */
-function injectIf<T, R>(predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>, cb: (current: T) => Iterable<R>): Operation<T, T | R> {
+function injectIf<T>(predicate: (value: T, index: number, state: IterationState) => boolean | Promise<boolean>, cb: (current: T) => Iterable<T>): Operation<T, T> {
     return source => concurrencyFork({
         onSync(i: Iterable<T>) {
             const m = map((a: T, index, state) => predicate(a, index, state) ? cb(a) : [a])(i);
-            return spread()(m) as Iterable<T | R>;
+            return spread()(m) as Iterable<T>;
         },
         onAsync(i: AsyncIterable<T>) {
             const m = map((a: T, index, state) => {
@@ -39,9 +39,9 @@ function injectIf<T, R>(predicate: (value: T, index: number, state: IterationSta
                 const isPromise = r && typeof r.then === 'function';
                 const out = (pass: boolean) => pass ? cb(a) : [a];
                 return isPromise ? r.then(out) : out(r);
-            })(i) as AsyncIterable<Array<T | R> | Promise<Array<T | R>>>;
-            const w = wait()(m) as AsyncIterable<Array<T | R>>;
-            return spread()(w) as AsyncIterable<T | R>;
+            })(i);
+            const w = wait()(m) as AsyncIterable<Array<T>>;
+            return spread()(w) as AsyncIterable<T>;
         }
     })(source);
 }
