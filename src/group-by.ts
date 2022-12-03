94e1ff2fe1bc713b1979a418c8ffbfbe14ca252f
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-03 09:28:29 +0000

    docs

--- src/group-by.ts
+++ src/group-by.ts
@@ -4,6 +4,9 @@ export type GroupByResult<T> = { [key: string]: T[] };
 
 /**
  * Groups objects by a property value.
+ *
+ * It works the same as Array.group:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/group
  */
 export function groupBy<T>(predicate: (v: T) => string): Operation<T, GroupByResult<T>> {
     return reduce((acc: GroupByResult<T>, value: T) => {
