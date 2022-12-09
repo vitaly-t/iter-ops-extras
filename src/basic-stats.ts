8623e6749cdcb677dde2c3c5d34ac64817d8d75d
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-09 17:19:18 +0000

    improving operator basicStats

--- src/basic-stats.ts
+++ src/basic-stats.ts
@@ -1,20 +1,27 @@
 import {Operation, reduce} from 'iter-ops';
 
-export type BasicStatsResult<T> = { min?: T, max?: T, avg?: T, sum?: T };
+export type BasicStatsResult = {
+    min?: number, // minimum value (undefined, when count is 0)
+    max?: number, // maximum value (undefined, when count is 0)
+    avg?: number, // average value (undefined, when count is 0)
+    sum?: number, // sum of all values (undefined, when count is 0)
+    count: number // count
+};
 
-export function basicStats<T>(): Operation<T, BasicStatsResult<T>>;
-export function basicStats<T, R>(keySelector?: (value: T) => R): Operation<T, BasicStatsResult<R>>;
+export function basicStats<T extends number>(): Operation<T, BasicStatsResult>;
+export function basicStats<T, R extends number>(keySelector: (value: T) => R): Operation<T, BasicStatsResult>;
 
 /**
- * Emits basic statistics `{min, max, avg, sum}`, with optional key selector.
+ * Emits basic statistics `{min, max, avg, sum, count}`, with optional key selector.
  */
-export function basicStats<T, R>(keySelector?: (value: T) => R): Operation<T, BasicStatsResult<R>> {
-    return reduce((p: any, c: T, idx: number) => {
+export function basicStats<T, R>(keySelector?: (value: T) => R): Operation<T, BasicStatsResult> {
+    return reduce((p: any, c: T) => {
         const a = keySelector ? keySelector(c) : c;
+        p.count++;
         p.sum = (p.sum ?? 0) + a;
-        p.avg = p.sum / (idx + 1);
+        p.avg = p.sum / p.count;
         p.min = a < p.min ? a : p.min ?? a;
         p.max = a > p.max ? a : p.max ?? a;
         return p;
-    }, {min: undefined, max: undefined, avg: undefined, sum: undefined});
+    }, {min: undefined, max: undefined, avg: undefined, sum: undefined, count: 0});
 }
