5f31e047fe5708b829a03bd647dcd7b052fbb5dc
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-10 13:09:59 +0000

    refactoring

--- src/remove-nil.ts
+++ src/remove-nil.ts
@@ -3,6 +3,6 @@ import {filter, Operation} from 'iter-ops';
 /**
  * Removes all `null` and `undefined` values.
  */
-export function removeNil<T>(): Operation<T | undefined | null, T> {
+export function removeNil<T>(): Operation<T, NonNullable<T>> {
     return filter((a: any) => a !== null && a !== undefined);
 }
