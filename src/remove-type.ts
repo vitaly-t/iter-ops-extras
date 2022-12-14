5dd1d5dab2e239fb68b02db2ad0ecf5ae64d12f8
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-14 17:31:02 +0000

    refactoring remove-types

--- src/remove-type.ts
+++ src/remove-type.ts
@@ -1,43 +1,38 @@
 import {filter, Operation} from 'iter-ops';
 
-///////////////////////////////////////////////////////////
-// Filters out values of basic types, with type re-casting.
-///////////////////////////////////////////////////////////
-
 type PrimitiveMap = {
     string: string;
     number: number;
     bigint: bigint;
     boolean: boolean;
     symbol: symbol;
-    undefined: undefined;
     object: object;
+    undefined: undefined;
 }
 
 type PrimitivesAsStrings = keyof PrimitiveMap;
 type Primitives = PrimitiveMap[PrimitivesAsStrings];
 
 /**
- * Removes elements of specified type(s), and re-casts the type.
+ * Removes elements of specified type(s), and re-casts the type:
  *
- * ```ts
  * pipe([1, 2, 'three', false], removeType('string', 'boolean')) //=> Iterable<number>
- * ```
  */
-export function removeType<T extends Primitives, R extends PrimitivesAsStrings>(...t: R[]): Operation<T, Exclude<T, PrimitiveMap[R]>> {
-    return filter(a => t.indexOf(typeof a as any) < 0) as any;
+export function removeType<T>(): Operation<T, T>;
+export function removeType<T extends Primitives, R extends PrimitivesAsStrings>(...t: R[]): Operation<T, Exclude<T, PrimitiveMap[R]>>;
+
+export function removeType(...t: string[]): any {
+    return filter(a => t.indexOf(typeof a) < 0);
 }
 
 /**
- * Removes elements that are not of specified type(s), and re-casts the type.
+ * Removes elements that are not of specified type(s), and re-casts the type:
  *
- * ```ts
  * pipe([1, 2, 'three', false], removeNotType('number', 'boolean')) //=> Iterable<number|boolean>
- * ```
- *
- * LIMITATION: If you use this function with 'object', any specific object type will become generic 'object',
- *             so you will then need to re-cast it downstream.
  */
-export function removeNotType<T extends Primitives, R extends PrimitivesAsStrings>(...t: R[]): Operation<T, PrimitiveMap[R]> {
-    return filter(a => t.indexOf(typeof a as any) >= 0) as any;
+export function removeNotType<T>(): Operation<T, T>;
+export function removeNotType<T extends Primitives, R extends PrimitivesAsStrings>(...t: R[]): Operation<T, PrimitiveMap[R]>;
+
+export function removeNotType(...t: string[]): any {
+    return filter(a => t.indexOf(typeof a) >= 0);
 }
