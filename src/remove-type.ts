b56312fba243683f6c98d56f5d70cdbca8ac2ec9
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-15 19:56:56 +0000

    improving removeType

--- src/remove-type.ts
+++ src/remove-type.ts
@@ -13,14 +13,12 @@ type PrimitiveMap = {
 type PrimitivesAsStrings = keyof PrimitiveMap;
 type Primitives = PrimitiveMap[PrimitivesAsStrings];
 
-export function removeType<T>(): Operation<T, T>;
+export function removeType<T>(): Operation<T, unknown>;
 
 /**
  * Removes values of specified basic type(s), with recasting:
  *
  * pipe([1, 2, 'three', false], removeType('string', 'boolean')) //=> Iterable<number>
- *
- * When used without parameters, it will do nothing.
  */
 export function removeType<T extends Primitives, R extends PrimitivesAsStrings>(...t: R[]): Operation<T, Exclude<T, PrimitiveMap[R]>>;
 
@@ -28,14 +26,12 @@ export function removeType(...t: string[]): any {
     return filter(a => t.indexOf(typeof a) < 0);
 }
 
-export function removeNotType<T>(): Operation<T, never>;
+export function removeNotType<T>(): Operation<T, unknown>;
 
 /**
  * Removes values that are not of specified type(s), with recasting:
  *
  * pipe([1, 2, 'three', false], removeNotType('number', 'boolean')) //=> Iterable<number | boolean>
- *
- * When used without parameters, it will remove everything.
  */
 export function removeNotType<T extends Primitives, R extends PrimitivesAsStrings>(...t: R[]): Operation<T, PrimitiveMap[R]>;
 
