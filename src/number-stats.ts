b8c4c27a3b9b47c4f2d391822e4c41ba22c4690b
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-15 14:53:22 +0000

    improving operator numberStats

--- src/number-stats.ts
+++ src/number-stats.ts
@@ -1,20 +1,20 @@
 import {Operation, reduce} from 'iter-ops';
 
-export type NumberStatsResult = {
-    min?: number, // minimum value (undefined, when count is 0)
-    max?: number, // maximum value (undefined, when count is 0)
-    avg?: number, // average value (undefined, when count is 0)
-    sum?: number, // sum of all values (undefined, when count is 0)
-    count: number // count
+export type NumberStatsResult<T> = {
+    min?: T, // minimum value (undefined, when count is 0)
+    max?: T, // maximum value (undefined, when count is 0)
+    avg?: T, // average value (undefined, when count is 0)
+    sum?: T, // sum of all values (undefined, when count is 0)
+    count: T // count
 };
 
-export function numberStats<T extends number>(): Operation<T, NumberStatsResult>;
-export function numberStats<T, R extends number>(keySelector: (value: T) => R): Operation<T, NumberStatsResult>;
+export function numberStats(): Operation<number, NumberStatsResult<number>>;
+export function numberStats<T>(keySelector: (value: T) => number): Operation<T, NumberStatsResult<number>>;
 
 /**
  * Emits basic number statistics `{min, max, avg, sum, count}`, with optional key selector.
  */
-export function numberStats<T, R>(keySelector?: (value: T) => R): Operation<T, NumberStatsResult> {
+export function numberStats<T>(keySelector?: (value: T) => number): Operation<T, NumberStatsResult<number>> {
     return reduce((p: any, c: T) => {
         const a = keySelector ? keySelector(c) : c;
         p.count++;
@@ -24,4 +24,22 @@ export function numberStats<T, R>(keySelector?: (value: T) => R): Operation<T, N
         p.max = a > p.max ? a : p.max ?? a;
         return p;
     }, {min: undefined, max: undefined, avg: undefined, sum: undefined, count: 0});
+}
+
+export function bigNumberStats(): Operation<bigint, NumberStatsResult<bigint>>;
+export function bigNumberStats<T>(keySelector: (value: T) => bigint): Operation<T, NumberStatsResult<bigint>>;
+
+/**
+ * Emits basic number statistics `{min, max, avg, sum, count}`, with optional key selector.
+ */
+export function bigNumberStats<T>(keySelector?: (value: T) => bigint): Operation<T, NumberStatsResult<bigint>> {
+    return reduce((p: any, c: T) => {
+        const a = keySelector ? keySelector(c) : c;
+        p.count++;
+        p.sum = (p.sum ?? 0n) + a;
+        p.avg = p.sum / p.count;
+        p.min = a < p.min ? a : p.min ?? a;
+        p.max = a > p.max ? a : p.max ?? a;
+        return p;
+    }, {min: undefined, max: undefined, avg: undefined, sum: undefined, count: 0n});
 }
