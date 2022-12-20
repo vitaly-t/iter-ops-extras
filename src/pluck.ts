1525175b4ebeceb3a01c3ad32dce88d2dc59aafc
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-20 18:52:08 +0000

    refactoring pluck

--- src/pluck.ts
+++ src/pluck.ts
@@ -1,5 +1,6 @@
 import {map, Operation} from 'iter-ops';
 
+export function pluck<T>(): Operation<T, T>;
 export function pluck<T, K1 extends keyof T>(k1: K1): Operation<T, T[K1]>;
 export function pluck<T, K1 extends keyof T, K2 extends keyof T[K1]>(k1: K1, k2: K2): Operation<T, T[K1][K2]>;
 export function pluck<T, K1 extends keyof T, K2 extends keyof T[K1], K3 extends keyof T[K1][K2]>(k1: K1, k2: K2, k3: K3): Operation<T, T[K1][K2][K3]>;
@@ -34,10 +35,5 @@ export function pluck<T>(...properties: string[]): Operation<T, unknown>;
  * Maps each value to its specified nested property.
  */
 export function pluck<T, R>(...props: string[]): Operation<T, R> {
-    return map(a => {
-        if (!props.length) {
-            throw new TypeError('List of properties cannot be empty.');
-        }
-        return props.reduce((p: any, c: string) => p && p[c], a);
-    });
+    return map(a => props.reduce((p: any, c: string) => p && p[c], a));
 }
