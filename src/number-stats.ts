b8713a1648b381695ff23889552def879852e93f
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2023-08-02 09:21:06 +0100

    refactoring numberStats

--- src/number-stats.ts
+++ src/number-stats.ts
@@ -21,16 +21,7 @@ export function numberStats<T>(keySelector: (value: T) => number): Operation<T, 
  * Emits basic `number` statistics `{min, max, avg, sum, count}`, with optional key selector.
  */
 export function numberStats<T>(keySelector?: (value: T) => number): Operation<number, NumberStatsResult<number>> {
-    const hasSelector = typeof keySelector === 'function';
-    return reduce((p: any, c: number) => {
-        const a = hasSelector ? keySelector(c as T) : c;
-        p.count++;
-        p.sum = (p.sum ?? 0) + a;
-        p.avg = p.sum / p.count;
-        p.min = a < p.min ? a : p.min ?? a;
-        p.max = a > p.max ? a : p.max ?? a;
-        return p;
-    }, {min: undefined, max: undefined, avg: undefined, sum: undefined, count: 0});
+    return buildStats(keySelector, true);
 }
 
 export function bigIntStats(): Operation<bigint, NumberStatsResult<bigint>>;
@@ -40,14 +31,19 @@ export function bigIntStats<T>(keySelector: (value: T) => bigint): Operation<T, 
  * Emits basic `bigint` statistics `{min, max, avg, sum, count}`, with optional key selector.
  */
 export function bigIntStats<T>(keySelector?: (value: T) => bigint): Operation<bigint, NumberStatsResult<bigint>> {
+    return buildStats(keySelector, false);
+}
+
+function buildStats<T>(keySelector?: (value: T) => number | bigint, isNumber = true): Operation<number | bigint, NumberStatsResult<any>> {
     const hasSelector = typeof keySelector === 'function';
-    return reduce((p: any, c: bigint) => {
+    const zero = isNumber ? 0 : 0n;
+    return reduce((p: any, c: number | bigint) => {
         const a = hasSelector ? keySelector(c as T) : c;
         p.count++;
-        p.sum = (p.sum ?? 0n) + a;
+        p.sum = (p.sum ?? zero) + a;
         p.avg = p.sum / p.count;
         p.min = a < p.min ? a : p.min ?? a;
         p.max = a > p.max ? a : p.max ?? a;
         return p;
-    }, {min: undefined, max: undefined, avg: undefined, sum: undefined, count: 0n});
+    }, {min: undefined, max: undefined, avg: undefined, sum: undefined, count: zero});
 }
