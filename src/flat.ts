29aac70c5d7265370342458c92862ab1c3066d7a
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2024-07-08 11:05:25 +0100

    Update flat.ts

--- src/flat.ts
+++ src/flat.ts
@@ -4,12 +4,12 @@ import {flat as _flat, map, IterationState} from 'iter-ops';
  * Replacement for the default `flat` and `flatMap` with ones that replicate `Array.flat`
  * and `Array.flatMap` precisely, in case such behaviour is preferred.
  *
- * The library's own implementation, from v3.2.0, expands all iterables, including strings.
+ * The library's own implementation expands all iterables, including strings.
  * This implementation simply makes strings non-expandable.
  */
 
 export function flat<T>(depth = 1) {
-    return _flat(depth, v => typeof v === 'string');
+    return _flat(depth, v => typeof v === 'string'); // use v3.2.0 'skip' callback
 }
 
 export function flatMap<T, R>(cb: (value: T, index: number, state: IterationState) => R | Promise<R>) {
